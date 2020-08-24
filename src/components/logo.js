a03da6a0abc397bf1ec4706382a68c51f34e8f14
jenniferbasford <basford.jennifer@gmail.com>
2020-08-24 17:11:47 -0600

    modified logo component to take in a custom size via props

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import DS_Logo from "../../static/assets/ds_circle_logo.png"
+import DS_Logo from "../../static/assets/ds_circle_logo.png";
 
 class Logo extends Component {
-    render() {
-        return (
-            <div className="logo-main">
-                <img src={DS_Logo}/>
-            </div>
-        )
-    }
+  render() {
+    const size = {
+      height: this.props.size ? this.props.size : 105,
+      width: this.props.size ? this.props.size : 105,
+    };
+    return (
+      <div className="logo-main">
+        <img style={size} src={DS_Logo} />
+      </div>
+    );
+  }
 }
 
 export default Logo;
